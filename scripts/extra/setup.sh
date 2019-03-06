fc8c6ac4abc3bc495774d9ec20887c84687c64b5
Ankush Behl <cloudbehl@gmail.com>
2019-03-06 13:25:07 +0530

    Update setup.sh

--- scripts/extra/setup.sh
+++ scripts/extra/setup.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-cd $GOPATH/src/github.com/openshift/installer/
+yum install git -y
+
+yum install go -y
+mkdir -p $HOME/go/src/github.com/openshift/
+echo "export GOPATH=$HOME/go"
+cd $HOME/go/src/github.com/openshift/
+
+git clone https://github.com/cloudbehl/installer.git -b installer-0.9
+
+cd installer/ && ./scripts/maintenance/install-deps.sh
+
 TAGS=libvirt hack/build.sh
+
 mkdir -p cluster
 bin/openshift-install --log-level=debug --dir=cluster create cluster
